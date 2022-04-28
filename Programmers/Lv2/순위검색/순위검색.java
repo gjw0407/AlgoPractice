cb77fdc1a54726937a20ab624c59c37a46ebc643
곽지원 <gjw0407@gmail.com>
2022-04-28 22:36:13 +0900

    Day 12, Prob 12, 13

---
+++ Programmers/Lv2/순위검색/순위검색.java
@@ -0,0 +1,81 @@
+package Programmers.Lv2.순위검색;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// time: 37:32
+// 2022.04.28
+public class 순위검색 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"},
+                new String[]{"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"}));
+    }
+
+    public static List<Integer> solution(String[] info, String[] query) {
+        List<Integer> answer = new ArrayList<>();
+
+        List<List<String>> list = new ArrayList<>();
+        List<List<String>> list2 = new ArrayList<>();
+        for(int i = 0; i < query.length; i++){
+            List<String> tempL = new ArrayList<>();
+            String temp = "";
+            for(int j = 0; j < query[i].length(); j++){
+                if(query[i].charAt(j) != ' '){
+                    temp += String.valueOf(query[i].charAt(j));
+                } else {
+                    if(temp.equals("and")){
+                        temp = "";
+                        continue;
+                    }
+                    tempL.add(temp);
+                    temp = "";
+                }
+            }
+            tempL.add(temp);
+            list.add(tempL);
+        }
+
+        for(int i = 0; i < info.length; i++){
+            List<String> tempL = new ArrayList<>();
+            String temp = "";
+            for(int j = 0; j < info[i].length(); j++){
+                if(info[i].charAt(j) != ' '){
+                    temp += String.valueOf(info[i].charAt(j));
+                } else{
+                    tempL.add(temp);
+                    temp = "";
+                }
+            }
+            tempL.add(temp);
+            list2.add(tempL);
+        }
+
+        for(int i = 0; i < list.size(); i++){
+            int ans = 0;
+        for(int j = 0; j < list2.size(); j++){
+                int cnt = 0;
+                for(int k = 0; k < 4; k++){
+                    if(!list.get(i).get(k).equals("-")){
+                        if(list.get(i).get(k).equals(list2.get(j).get(k))){
+                            cnt++;
+                        } else {
+                            break;
+                        }
+                    } else {
+                        cnt++;
+                    }
+                }
+                if(Integer.parseInt(list.get(i).get(4)) <= Integer.parseInt(list2.get(j).get(4))){
+                    cnt++;
+                }
+                if(cnt == 5){
+                    ans++;
+                }
+            }
+            answer.add(ans);
+        }
+
+        return answer;
+    }
+}
---
+++ Programmers/Lv2/순위검색/순위검색2.java
@@ -0,0 +1,68 @@
+package Programmers.Lv2.순위검색;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+// time: 37:32
+// 2022.04.28
+public class 순위검색2 {
+    static HashMap<String, List<Integer>> map;
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"},
+                new String[]{"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"}));
+    }
+
+    public static int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
+        map = new HashMap<>();
+
+        for (int i = 0; i < info.length; i++) {
+            String[] p = info[i].split(" ");
+            makeSentence(p, "", 0);
+        }
+
+        for (String key : map.keySet())
+            Collections.sort(map.get(key));
+
+        for (int i = 0; i < query.length; i++) {
+            query[i] = query[i].replaceAll(" and ", "");
+            String[] q = query[i].split(" ");
+            System.out.println(q[0]);
+            answer[i] = map.containsKey(q[0]) ? binarySearch(q[0], Integer.parseInt(q[1])) : 0;
+        }
+
+        return answer;
+    }
+
+    private static void makeSentence(String[] p, String str, int cnt) {
+        if (cnt == 4) {
+            if (!map.containsKey(str)) {
+                List<Integer> list = new ArrayList<Integer>();
+                map.put(str, list);
+            }
+            map.get(str).add(Integer.parseInt(p[4]));
+            return;
+        }
+        makeSentence(p, str + "-", cnt + 1);
+        makeSentence(p, str + p[cnt], cnt + 1);
+    }
+
+    private static int binarySearch(String key, int score) {
+        List<Integer> list = map.get(key);
+        System.out.println(list);
+        int start = 0, end = list.size() - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (list.get(mid) < score)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+
+        return list.size() - start;
+    }
+}
