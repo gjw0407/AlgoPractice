0a69ff467118993948675ce229f932709fc1644e
gjw0407 <gjw0407@gmail.com>
2022-06-06 22:52:55 +0900

    Day 30, Prob 31

---
+++ baekjoon/silver/S1_14888_연산자끼워넣기.java
@@ -0,0 +1,14 @@
+package baekjoon.silver;
+
+import java.util.Scanner;
+
+public class S1_14888_연산자끼워넣기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for(int i = 0; i < N; i++){
+
+        }
+    }
+}
