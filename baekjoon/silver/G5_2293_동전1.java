ff2a6c5d8ed6518389b1a02ad2cfc5c276d6c7fe
gjw0407 <gjw0407@gmail.com>
2022-06-11 13:55:35 +0900

    Day 34, Prob 37

---
+++ baekjoon/silver/G5_2293_동전1.java
@@ -0,0 +1,29 @@
+package baekjoon.silver;
+
+import java.util.Scanner;
+
+public class G5_2293_동전1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int coin[] = new int[n];
+        int dp[] = new int[k + 1];
+
+        for (int i = 0; i < n; i++) {
+            coin[i] = sc.nextInt();
+        }
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (j >= coin[i]) {
+                    dp[j] += dp[j - coin[i]];
+                }
+            }
+        }
+
+        System.out.println(dp[k]);
+
+    }
+}
