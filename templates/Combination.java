92db59758bf6da062258434ca66660646b26598b
gjw0407 <gjw0407@gmail.com>
2022-07-01 23:49:05 +0900

    practice algo

--- templates/Combination.java
+++ templates/Combination.java
@@ -1,18 +1,24 @@
 package templates;
 
+import java.util.Arrays;
+
 public class Combination {
+    static char[] src = {'a', 'b', 'c'};
+
     public static void main(String[] args) {
-        combination(1);
+        combination(2, 0, new char[2]);
     }
 
-    private static void combination(int r) {
-        if(r == 0){
+    private static void combination(int r, int start, char[] temp) {
+        if (r == 0) {
+            System.out.println(Arrays.toString(temp));
             return;
         }
 
-        for(int i = 0; i < 3; i++){
-
+        for (int i = start; i < src.length; i++) {
+            temp[r - 1] = src[i];
+            combination(r - 1, i + 1, temp);
         }
-
     }
+
 }
